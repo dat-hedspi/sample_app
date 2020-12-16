b1ffe7b1de31d94bafc6333a7d14cda4160e24d0
dat-hedspi <dat.hedspi@gmail.com>
2020-12-16 19:47:39 +0700

    static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+	root to: "static_pages#home"
+	get "/help", to: "static_pages#help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
